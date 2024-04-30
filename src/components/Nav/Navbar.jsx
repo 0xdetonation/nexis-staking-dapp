88fb8441f9e9b461ec7c25a0bf2310190f3972ba
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-04-30 17:16:34 +0530

    nexis logo

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Navbar.css"
 import AccountCard from '../Account/AccountCard';
+import logo from "../../assets/nexis.svg"
 
 function Navbar(props) {
   return (
     <div className="navbar">
-      <div className="navbar-left">Left Content</div>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+      </div>
       <div className="navbar-right">
         <AccountCard address={props.address} balance={props.balance}/>
       </div>
